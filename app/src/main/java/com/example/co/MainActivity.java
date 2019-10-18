2428e70fb25b642c8915a4f26df92223d11f31b2
maxi <maximilianoromeromm@gmail.com>
2019-10-17 21:36:13 -0300

    Initial commit

---
+++ app/src/main/java/com/example/co/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.co;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button b1, b2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        b1 = (Button) findViewById(R.id.button1);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                clic();
+            }
+        });
+
+        b2 = (Button) findViewById(R.id.button2);
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                clic();
+            }
+        });
+    }
+
+    private void clic(){
+        Intent intento = new Intent(MainActivity.this,pantalla2.class);
+        startActivity(intento);
+
+        Intent intentoa = new Intent(MainActivity.this,mapa.class);
+        startActivity(intento);
+    }
+}
---
+++ app/src/main/java/com/example/co/pantalla2.java
@@ -0,0 +1,14 @@
+package com.example.co;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class pantalla2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla2);
+    }
+}
